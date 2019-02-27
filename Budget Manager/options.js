6bb332a5792d276089ab5d26af02a9546ec0fa86
ShiviAgrawal113114 <agrawalshivi113114@gmail.com>
2019-02-28 01:49:47 +0530

    Two Extensions

---
+++ Budget Manager/options.js
@@ -0,0 +1,37 @@
+$(function()
+{
+	 chrome.storage.sync.get('limit', function(budget)
+	 {
+			$('#limit').val(budget.limit);
+     });
+	$('#saveLimit').click(function()
+	{
+		var limit = $('#limit').val();
+		if(limit)
+		{
+			chrome.storage.sync.set({'limit': limit}, function()
+			{
+			close();
+			});
+		}
+	});
+        
+        $('#resetTotal').click(function()
+        {
+        	chrome.storage.sync.set({'total': 0},  function()
+        	{
+				 
+					var notifOptions1 =
+					{
+						type: 'basic',
+						iconUrl: 'icon48.png',
+						title: 'Total reset!',
+						message: "Total has been reset to 0!"
+					};
+					chrome.notifications.create('limitNotif1', notifOptions1);
+		    });
+
+
+        });
+
+});
